9e6dec385ca28efa60deeef4cba6c33ab72592bd
BrooitsFeiskJR <luizantonio0125@gmail.com>
2025-02-16 11:21:17 -0300

    feat: create user dto and create function to create user inside user service

--- src/application/services/user_service.test.ts
+++ src/application/services/user_service.test.ts
@@ -1,28 +1,50 @@
 import { UserService } from "../services/user_service"
 import { FakeUserRepository } from "../../infrastructure/repositories/fake_user_repository"
 import { User } from "../../domain/entities/user";
+import { Repository, DataSource } from "typeorm";
+import { UserEntity } from "../../infrastructure/persistence/entities/user_entity";
+import { TypeORMUserRepository } from "../../infrastructure/repositories/typeorm_user_repository";
+import { CreateUserDTO } from "../dtos/create_user_dto";
 
 describe("User Service", () => {
-  let userService: UserService;
+  let dataSource: DataSource;
+  let userServiceFake: UserService;
+  let userServiceTypORM: UserService;
   let fakeUserRepository: FakeUserRepository;
+  let userReposiotry: Repository<UserEntity>;
+  let typeORMUserRepository: TypeORMUserRepository
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    dataSource = new DataSource(
+          {
+            type: "sqlite",
+            database: ":memory",
+            dropSchema: true,
+            entities: [UserEntity],
+            synchronize: true,
+            logging: false,
+          }
+    );
+    await dataSource.initialize();
+    userReposiotry = dataSource.getRepository(UserEntity);
+    typeORMUserRepository = new TypeORMUserRepository(userReposiotry);
     fakeUserRepository = new FakeUserRepository();
-    userService = new UserService(fakeUserRepository);
+    userServiceFake = new UserService(fakeUserRepository);
+    userServiceTypORM = new UserService(typeORMUserRepository);
   })
   it("should return null when an invalid id passed", async () => {
-    const user = await userService.findUserById("999");
+    const user = await userServiceFake.findUserById("999");
     expect(user).toBeNull();
   });
 
   it("should return an user when an valid id passed", async () => {
-    const user = await userService.findUserById("1");
+    const user = await userServiceFake.findUserById("1");
     expect(user).not.toBeNull();
     expect(user?.getId()).toBe("1")
     expect(user?.getName()).toBe("John Doe")
   });
 
-  it("create a new user in repository and search", async () => {
+  it("create a new user in fake repository and search", async () => {
     const newUser = new User("3", "Test User");
     await fakeUserRepository.save(newUser);
 
@@ -30,6 +52,32 @@ describe("User Service", () => {
     expect(user).not.toBeNull();
     expect(user?.getId()).toBe("3");
     expect(user?.getName()).toBe("Test User");
+  });
+
+  it("create a new user in typerom repository and search", async () => {
+    const newUser = new User("3", "Test User");
+    await typeORMUserRepository.save(newUser);
+
+    const user = await typeORMUserRepository.findUserById("3")
+    expect(user).not.toBeNull();
+    expect(user?.getId()).toBe("3");
+    expect(user?.getName()).toBe("Test User");
+  });
+
+  it("should return an user when an valid id passed", async () => {
+    const newUser = new User("1", "Test User");
+    await typeORMUserRepository.save(newUser);
+    const user = await userServiceTypORM.findUserById("1");
+    expect(user).not.toBeNull();
+    expect(user?.getId()).toBe("1")
+    expect(user?.getName()).toBe("Test User")
+  });
+  it("create a new user usign user service with userdto", async () => {
+    const dto: CreateUserDTO = {id: "1", name: "Test User"};
+    const user = await userServiceTypORM.createUser(dto);
+    expect(user).not.toBeNull();
+    expect(user?.getId()).toBe("1")
+    expect(user?.getName()).toBe("Test User")
   });
 });
 
--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,5 +1,6 @@
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user_repository";
+import { CreateUserDTO } from "../dtos/create_user_dto";
 
 export class UserService {
   private readonly userRepository: UserRepository
@@ -10,6 +11,15 @@ export class UserService {
 
   async findUserById(id: string): Promise<User | null> {
     return this.userRepository.findUserById(id);
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<User> {
+    const user = new User(
+      dto.id,
+      dto.name
+    );
+    await this.userRepository.save(user);
+    return user;
   }
 }
 
